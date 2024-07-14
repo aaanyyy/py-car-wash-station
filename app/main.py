380133bb3dd70b8186b7f16ca6d4a83bbd2ab1c8
Andrii Uvarov <aaanyyy@gmail.com>
2024-07-14 10:31:22 +0200

    fixing flake8 problems

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self, comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
+
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -25,7 +28,8 @@ class CarWashStation:
     def calculate_washing_price(self, car: Car) -> float:
         if car.clean_mark >= self.clean_power:
             return 0
-        result = (car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating
+        result = (car.comfort_class * (self.clean_power - car.clean_mark)
+                  * self.average_rating
                   / self.distance_from_city_center)
         result = round(result, 1)
         return result
@@ -35,7 +39,9 @@ class CarWashStation:
             car.clean_mark = self.clean_power
 
     def rate_service(self, rate: float) -> None:
-        self.average_rating = (self.average_rating * self.count_of_ratings + rate) / (self.count_of_ratings + 1)
+        self.average_rating = ((self.average_rating * self.count_of_ratings
+                                + rate)
+                               / (self.count_of_ratings + 1))
         self.count_of_ratings = self.count_of_ratings + 1
         self.average_rating = round(self.average_rating, 1)
 
